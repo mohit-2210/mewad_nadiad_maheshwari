4c19ad8baca06bd482a65c2f9c0b4a96789ecd46
Mohit <mohitmaheshwari2210@gmail.com>
2025-11-27 00:57:14 +0530

    Image name changes

--- lib/app/globals/AppImages.dart
+++ lib/app/globals/AppImages.dart
@@ -4,37 +4,79 @@ class AppImages {
   static const String carouselImages2 = 'assets/carouselImages2.jpeg';
   static const String carouselImages3 = 'assets/carouselImages3.jpeg';
   static const String carouselImages4 = 'assets/carouselImages4.jpeg';
-  
+
   // Profile Images
   static const String profileImageHead1 = 'assets/profileImageHead1.jpeg';
-  static const String profileImageHead1Wife = 'assets/profileImageHead1Wife.jpeg';
+  static const String profileImageHead1Wife =
+      'assets/profileImageHead1Wife.jpeg';
   static const String profileImageHead1Son = 'assets/profileImageHead1Son.jpeg';
-  static const String profileImageHead1Daughter = 'assets/profileImageHead1Daughter.jpeg';
+  static const String profileImageHead1Daughter =
+      'assets/profileImageHead1Daughter.jpeg';
 
   static const String profileImageHead2 = 'assets/profile_head2.jpeg';
-
 
   static const String profileAvatar = 'assets/carouselImages1.jpeg';
   static const String logo = 'assets/AppIcon.png';
   static const String background = 'assets/images/bg.png';
 
-
   // Global People List (local assets)
   static const List<Map<String, String>> peopleList = [
-    {"name": "John", "image": "assets/AppIcon.webp"},
-    {"name": "Emma", "image": "assets/AppIcon.webp"},
-    {"name": "Alex", "image": "assets/AppIcon.webp"},
-    {"name": "Sophia", "image": "assets/AppIcon.webp"},
-    {"name": "David", "image": "assets/AppIcon.webp"},
-    {"name": "Mia", "image": "assets/AppIcon.webp"},
-    {"name": "Liam", "image": "assets/AppIcon.webp"},
-    {"name": "Liam", "image": "assets/AppIcon.webp"},
-    {"name": "Liam", "image": "assets/AppIcon.webp"},
-    {"name": "Liam", "image": "assets/AppIcon.webp"},
-    {"name": "Liam", "image": "assets/AppIcon.webp"},
-    {"name": "Olivia", "image": "assets/people/olivia.webp"},
+    {
+      "name": "Satynarayan Heda",
+      "image": "assets/people/satyanarayan_heda.webp",
+    },
+    {
+      "name": "Satish ajmera",
+      "image": "assets/people/satish_ajmera.webp",
+    },
+    {
+      "name": "Vinod jagetiya",
+      "image": "assets/people/vinod_jagetiya.webp",
+    },
+    {
+      "name": "Satish soni",
+      "image": "assets/people/satish_soni.webp",
+    },
+    {
+      "name": "Bharat heda",
+      "image": "assets/people/bharat_heda.webp",
+    },
+    {
+      "name": "Pradip asawa",
+      "image": "assets/people/pradip_asawa.webp",
+    },
+    {
+      "name": "Dipak inani",
+      "image": "assets/people/dinesh_inani.webp",
+    },
+    {
+      "name": "Anil toshniwal",
+      "image": "assets/people/anil_toshniwal.webp",
+    },
+    {
+      "name": "Vimal heda",
+      "image": "assets/people/vimal_heda.webp",
+    },
+    {
+      "name": "Ratnesh toshniwal",
+      "image": "assets/people/ratnesh_toshniwal.webp",
+    },
+    {
+      "name": "Naresh toshniwal",
+      "image": "assets/people/naresh_toshniwal.webp",
+    },
+    {
+      "name": "Nitin heda",
+      "image": "assets/people/nitin_heda.webp",
+    },
+    {
+      "name": "Vikas toshniwal",
+      "image": "assets/people/vikas_toshniwal.webp",
+    },
+    {
+      "name": "Dinesh khatod",
+      "image": "assets/people/dinesh_khatod.webp",
+    },
     // add as many people as needed (scalable)
   ];
-
-
 }
